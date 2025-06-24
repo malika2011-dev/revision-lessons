f25194f845b4c216402e8e69bba829ad2c22cc64
Malika Abdulahadova <malikaabdulahadova758@gmail.com>
2025-06-24 20:22:33 +0500

    datetime moduli

--- datetime_moduli.py
+++ datetime_moduli.py
@@ -21,3 +21,41 @@ x = int(input("Haftani kiriting: "))
 sana = bugun + timedelta(weeks=x)
 
 print(sana.strftime(f"{x} haftadan keyingi sana %m - %B %Y - yil. Hafatning %A kuni bo'ladi "))
+
+
+"""vazifa"""
+
+"""1-mashq"""
+
+sana = date(2010, 3, 10)
+keyingi_sana = sana - timedelta(weeks =8, hours= 325, minutes=854)
+
+print(f"{(keyingi_sana)}")
+
+
+"""2-mahshq"""
+
+yil = int(input("Yilni kiriitng (maktabga birinchi borgan): "))
+oy = int(input("Oyni kiriting (maktabga birinchi borgan): "))
+kun = int(input("Kunni kiriitng (maktabga birinchi borgan): "))
+
+birinchi_qongiroq = date(yil, oy, kun)
+farq = (bugun - birinchi_qongiroq).days
+
+print(f"Maktabga birinchi borganingnizga {farq} kun bo'libdiyey`")
+
+"""3-mashq"""
+
+try:
+    soat = int(input("Sotni kriitng: "))
+
+    keyingi_vaqt = bugun + timedelta(hours= soat)
+    print(keyingi_vaqt.strftime(f"{soat} soatdan keyin vaqt %H:%M  bo'ladi."))
+
+except ValueError:
+    print("Sanani raqamlarda kriitng: ")
+except OverflowError:
+    print("Siz chegaradan chqib ketdingiz")
+except:
+    print("Funksiya ishlashida xatolik yuz berdi")
+
